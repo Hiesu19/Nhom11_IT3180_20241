8a2cf8a60e7c29061f3d16071aa919f491615885
Duongdeptrai <duongthcsvt7e@gmail.com>
2024-10-23 16:52:27 +0700

    update

--- src/server/src/config/db/index.js
+++ src/server/src/config/db/index.js
@@ -2,14 +2,14 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 
 async function connect() {
-    dotenv.config();
+  dotenv.config();
 
-    try {
-        await mongoose.connect(process.env.MONGODB_URL);
-        console.log("Connect MongoDB OK");
-    } catch (error) {
-        console.log("False: MongoDB");
-    }
+  try {
+    await mongoose.connect(process.env.MONGODB_URL_ON);
+    console.log("Connect MongoDB OK");
+  } catch (error) {
+    console.log("False: MongoDB");
+  }
 }
 
 module.exports = { connect };
