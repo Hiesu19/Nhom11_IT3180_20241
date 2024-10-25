66176f1b49ff10bc329d6582c64a23d10c19818a
Nguyen Thai Hieu <87901713+Hiesu19@users.noreply.github.com>
2024-10-25 16:35:00 +0700

    update: mongoDB

--- src/server/src/routers/user.js
+++ src/server/src/routers/user.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const userController = require("../app/controllers/UserController");
 const middlewareControllers = require("../app/controllers/middlewareController");
 
-router.get("/", middlewareControllers.verifyToken, userController.getAllUsers);
+router.get("/", userController.getAllUsers); //, middlewareControllers.verifyToken
 router.delete(
     "/:id",
     middlewareControllers.verifyTokenAndAdmin,
