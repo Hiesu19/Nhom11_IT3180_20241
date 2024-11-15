a4ac8277a259e4236f6a2c5002901f40f5dec05c
Nguyen Thai Hieu <87901713+Hiesu19@users.noreply.github.com>
2024-11-15 14:44:27 +0700

    feat: add /createPaymentLink

---
+++ src/server/src/app/controllers/PaymentController.js
@@ -0,0 +1,52 @@
+const PayOS = require("@payos/node");
+class PaymentController {
+    // Tạo dạng yyyymmddhhmmss
+    formatDate = (date) => {
+        const y = date.getFullYear();
+        const m = String(date.getMonth() + 1).padStart(2, "0");
+        const d = String(date.getDate()).padStart(2, "0");
+        const h = String(date.getHours()).padStart(2, "0");
+        const min = String(date.getMinutes()).padStart(2, "0");
+        const s = String(date.getSeconds()).padStart(2, "0");
+        return parseInt(`${y}${m}${d}${h}${min}${s}`, 10);
+    };
+
+    createPaymentLink = async (req, res) => {
+        const payOS = new PayOS(
+            process.env.PAY_OS_CLIENT_ID,
+            process.env.PAY_OS_API_KEY,
+            process.env.PAY_OS_CHECKSUM_KEY
+        );
+
+        // const YOUR_DOMAIN = `http://localhost:3000`;
+        // Lấy domain từ request
+        const YOUR_DOMAIN = `${req.protocol}://${req.headers.host}`;
+
+        const { amount } = req.body;
+
+        const orderCode = this.formatDate(new Date());
+
+        const body_pay = {
+            orderCode: orderCode, // Tạo mã đơn hàng
+            amount: amount,
+            expiredAt: Math.floor(Date.now() / 1000) + 600,
+            description: `DON ${orderCode}`,
+            returnUrl: `${YOUR_DOMAIN}?success=true`,
+            cancelUrl: `${YOUR_DOMAIN}?canceled=true`,
+        };
+
+        try {
+            const paymentLinkResponse = await payOS.createPaymentLink(body_pay);
+            res.status(200).json({
+                paymentLink: paymentLinkResponse.checkoutUrl,
+                returnUrl: body_pay.returnUrl,
+                cancelUrl: body_pay.cancelUrl,
+            });
+        } catch (error) {
+            console.error(error);
+            res.status(500).send({ message: "Some thing error at payment" });
+        }
+    };
+}
+
+module.exports = new PaymentController();
--- src/server/src/routers/index.js
+++ src/server/src/routers/index.js
@@ -3,7 +3,7 @@ const userRouter = require("./user");
 const productRouter = require("./product");
 const dashboardRouter = require("./dashboard");
 const notificationRouter = require("./notification");
-
+const paymentRouter = require("./payment");
 
 function route(app) {
     app.use("/v1/auth", authRouter);
@@ -11,6 +11,6 @@ function route(app) {
     app.use("/v1/app/products", productRouter);
     app.use("/v1/app/dashboard", dashboardRouter);
     app.use("/v1/app/notification", notificationRouter);
-
+    app.use("/v1/app/payment", paymentRouter);
 }
 module.exports = route;
