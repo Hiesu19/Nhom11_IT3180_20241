94c0cfb25ad95af99779933e31fb08a0c0f4b3be
Nguyen Thai Hieu <87901713+Hiesu19@users.noreply.github.com>
2024-12-09 14:35:36 +0700

    update: new Dashboard

--- src/client/src/Components/Page/Dashboard/Dashboard.jsx
+++ src/client/src/Components/Page/Dashboard/Dashboard.jsx
@@ -1,23 +1,21 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import {
-    CubeIcon,
-    ShoppingCartIcon,
-    CurrencyDollarIcon,
-    UsersIcon,
-    InboxStackIcon,
-} from "@heroicons/react/24/outline";
 
-import RevenueChart from "./RevenueChart";
+
+import AdminDashboard from "./AdminDashboard";
+import QLKDashboard from "./QLKDashboard";
+import NVBanHangDashboard from "./NVBanHangDashboard";
 
 const Dashboard = () => {
-    const [overviewData, setOverviewData] = useState(null); // Khởi tạo là null
+    const [info, setInfo] = useState({});
+    const [overviewData, setOverviewData] = useState(null);
     const [error, setError] = useState(null);
 
     useEffect(() => {
         const fetchData = async () => {
             try {
                 const token = JSON.parse(localStorage.getItem("user"));
+                setInfo(token.user_datas);
                 const response = await axios.get(
                     "http://localhost:8000/v1/app/dashboard",
                     {
@@ -26,12 +24,9 @@ const Dashboard = () => {
                         },
                     }
                 );
-
-                const data = response.data;
-
-                setOverviewData(data);
+                setOverviewData(response.data);
             } catch (err) {
-                console.error("Error fetching overview data:", err);
+                console.error("Error fetching data:", err);
                 setError(
                     "Lỗi khi lấy dữ liệu từ server. Vui lòng thử lại sau."
                 );
@@ -41,74 +36,25 @@ const Dashboard = () => {
         fetchData();
     }, []);
 
-    if (error) {
-        return <div>{error}</div>;
-    }
-
-    if (!overviewData) {
-        return <div>Loading...</div>;
-    }
-    console.log(overviewData);
+    if (error) return <div>{error}</div>;
+    if (!overviewData) return <div>Loading...</div>;
 
     return (
         <div className="min-h-screen bg-gray-100 p-6">
-            {/* Header */}
             <header className="bg-white shadow p-4 rounded-lg mb-6 flex justify-between items-center">
                 <h1 className="text-2xl font-bold text-gray-800">Dashboard</h1>
+                <h1 className="text-2xl text-blue-600">{info.name} !!!</h1>
             </header>
 
-            {/* Hiển thị lỗi nếu có */}
-            {error && <p className="text-red-500 mb-4">{error}</p>}
-
-            {/* Overview Cards */}
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 mb-6">
-                <OverviewCard
-                    color="blue"
-                    icon={<CubeIcon className="h-6 w-6 text-blue-500" />}
-                    label="Số Loại Sản Phẩm"
-                    value={overviewData.productCount}
-                />
-                <OverviewCard
-                    color="green"
-                    icon={
-                        <CurrencyDollarIcon className="h-6 w-6 text-green-500" />
-                    }
-                    label="Giá Trị Tồn Kho"
-                    value={`$${overviewData.totalValue}`}
-                />
-                <OverviewCard
-                    color="yellow"
-                    icon={
-                        <InboxStackIcon className="h-6 w-6 text-yellow-500" />
-                    }
-                    label="Tổng Số Sản Phẩm "
-                    value={overviewData.totalProduct}
-                />
-                <OverviewCard
-                    color="red"
-                    icon={<UsersIcon className="h-6 w-6 text-red-500" />}
-                    label="Tổng Số Nhân Viên"
-                    value={overviewData.userCount}
-                />
-            </div>
-            {/* <RevenueChart data={overviewData.invoiceAndRevenueByDate} /> */}
-        </div>
-    );
-};
-
-// Component cho các thẻ Overview
-const OverviewCard = ({ color, icon, label, value }) => {
-    return (
-        <div className="bg-white p-4 rounded-lg shadow-md flex items-center">
-            <div
-                className={`p-3 rounded-full bg-${color}-100 text-${color}-500 mr-4`}
-            >
-                {icon}
-            </div>
-            <div>
-                <h4 className="text-gray-600">{label}</h4>
-                <p className="text-2xl font-bold">{value}</p>
-            </div>
+            {info.role === "admin" && (
+                <AdminDashboard overviewData={overviewData} />
+            )}
+            {info.role === "ql_kho" && (
+                <QLKDashboard overviewData={overviewData} />
+            )}
+            {info.role === "nv_ban_hang" && (
+                <NVBanHangDashboard overviewData={overviewData} />
+            )}
         </div>
     );
 };
